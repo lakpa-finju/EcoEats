90c5832ee475c0ec42e23b99468b785418dc348e
Lakpa Sherpa <lakpasherpa@Lakpas-MBP.ad.wlu.edu>
2024-05-12 11:14:27 -0400

    set up the directory structures.

--- index.js
+++ index.js
@@ -6,4 +6,5 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+export { default as HomeScreen } from './src/screens/HomeScreen/HomeScreen'
 AppRegistry.registerComponent(appName, () => App);
